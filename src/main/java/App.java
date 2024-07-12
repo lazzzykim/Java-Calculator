c6dc27057332a7a72dfc1618305baf77e60bb65e
lazzzykim <33sunghyeon@gmail.com>
2024-07-12 16:28:07 +0900

    Lv1)요구사항 3. 입력받은 수와 사칙연산 기호로 결과값 계산 후 출력

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,17 +4,35 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int firNum;
-        int secondNum;
+        int num1;
+        int num2;
         int result;
 
         System.out.println("첫번째 숫자 입력");
-        firNum = scanner.nextInt();
-
-        System.out.println("사칙연산 기호 입력 (+, -, *, /");
-        char operator = scanner.next().charAt(0);
+        num1 = scanner.nextInt();
 
         System.out.println("두번째 숫자 입력");
-        secondNum = scanner.nextInt();
+        num2 = scanner.nextInt();
+
+        System.out.println("사칙연산 기호 입력 (+, -, *, /)");
+        char operator = scanner.next().charAt(0);
+
+        switch (operator) {
+            case '+' :
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("유효하지 않은 연산자입니다.");
+                return;
+        }
+        System.out.println("결과: " + result);
     }
 }
