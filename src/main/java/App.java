734801f62a052f55b333910a365a6cf60132c76f
lazzzykim <33sunghyeon@gmail.com>
2024-07-12 16:53:13 +0900

    Lv1)요구사항 3 수정. 나누기 연산자 시 분모에 0이 올 경우에 대해 예외처리 요구사항 4. exit 문자열 입력 시 프로그램 종료 로직 작성

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,33 +6,48 @@ public class App {
 
         int num1;
         int num2;
-        int result;
+        int result = 0;
 
-        System.out.println("첫번째 숫자 입력");
-        num1 = scanner.nextInt();
 
-        System.out.println("두번째 숫자 입력");
-        num2 = scanner.nextInt();
+        while (true) {
+            System.out.println("첫번째 숫자 입력");
+            num1 = scanner.nextInt();
 
-        System.out.println("사칙연산 기호 입력 (+, -, *, /)");
-        char operator = scanner.next().charAt(0);
+            System.out.println("두번째 숫자 입력");
+            num2 = scanner.nextInt();
 
-        switch (operator) {
-            case '+' :
-                result = num1 + num2;
+            System.out.println("사칙연산 기호 입력 (+, -, *, /)");
+            char operator = scanner.next().charAt(0);
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                        continue;
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("유효하지 않은 연산자입니다.");
+                    continue;
+            }
+
+            System.out.println("결과: " + result);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            scanner.nextLine();
+            String endCalc = scanner.nextLine();
+
+            if (endCalc.equals("exit")) {
                 break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-            case '/':
-                result = num1 / num2;
-                break;
-            default:
-                System.out.println("유효하지 않은 연산자입니다.");
-                return;
+            }
         }
-        System.out.println("결과: " + result);
     }
 }
