590fb7bc5ab831d44f13782e8941418e722f6500
lazzzykim <33sunghyeon@gmail.com>
2024-07-12 15:55:44 +0900

    Lv1)요구사항 1. Scanner를 사용해서 숫자 입력 후 변수 초기화

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firNum;
+        int secondNum;
+        int result;
+
+        System.out.println("첫번째 숫자 입력");
+        firNum = scanner.nextInt();
+
+        System.out.println("두번째 숫자 입력");
+        secondNum = scanner.nextInt();
     }
 }
